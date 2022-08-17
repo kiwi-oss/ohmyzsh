59bdefb8fe3d531960b6eb9a234a891db94ed279
kiwi-oss <71145773+kiwi-oss@users.noreply.github.com>
2022-08-17 22:59:12 +0200

    Create completions directory before creating file in it

--- plugins/helm/helm.plugin.zsh
+++ plugins/helm/helm.plugin.zsh
@@ -4,10 +4,11 @@ fi
 
 # If the completion file does not exist, generate it and then source it
 # Otherwise, source it and regenerate in the background
-if [[ ! -f "$ZSH_CACHE_DIR/completions/_helm" ]]; then
-  helm completion zsh | tee "$ZSH_CACHE_DIR/completions/_helm" >/dev/null
-  source "$ZSH_CACHE_DIR/completions/_helm"
+if [[ ! -f "${ZSH_CACHE_DIR}/completions/_helm" ]]; then
+  mkdir --parents "${ZSH_CACHE_DIR}/completions"
+  helm completion zsh | tee "${ZSH_CACHE_DIR}/completions/_helm" >/dev/null
+  source "${ZSH_CACHE_DIR}/completions/_helm"
 else
-  source "$ZSH_CACHE_DIR/completions/_helm"
-  helm completion zsh | tee "$ZSH_CACHE_DIR/completions/_helm" >/dev/null &|
+  source "${ZSH_CACHE_DIR}/completions/_helm"
+  helm completion zsh | tee "${ZSH_CACHE_DIR}/completions/_helm" >/dev/null &|
 fi
